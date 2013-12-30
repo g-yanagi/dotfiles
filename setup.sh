d2487f3f5865caae24f09785dc6d71dd6b416fc4
g-yanagi <goro.yanagi@gmail.com>
2013-12-30 22:40:22 +0900

    add sequence to install ricty font

--- setup.sh
+++ setup.sh
@@ -6,3 +6,12 @@ for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
+
+
+# Ricty font の install
+brew tap sanemat/font
+brew install ricty
+
+cmd=$(brew info ricty | grep "Ricty\*.ttf" | sed -e "s/.*\(cp -f.*\)/\1/")
+echo $cmd
+eval $cmd
