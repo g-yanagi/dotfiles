3c313d82b505baef92f607ff16be17987be1616d
nutmeg-goro <goro.yanagi@nutmeglabs.com>
2021-12-13 18:34:31 +0900

    add setup scripts for ubuntu

---
+++ install_ubuntu_nvm.sh
@@ -0,0 +1,7 @@
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+
+source ~/.bashrc
+
+nvm install v14.17.5
+
+nvm alias default v14.17.5
---
+++ install_ubuntu_tools.sh
@@ -0,0 +1,11 @@
+sudo apt update
+apt install unzip
+apt install python3.8-venv
+sudo ln -s /usr/bin/python3 /usr/bin/python
+sudo snap install kubectl --classic
+
+sudo apt install make
+sudo snap install protobuf --classic
+sudo apt install clang-format
+
+sudo snap install helm --classic
